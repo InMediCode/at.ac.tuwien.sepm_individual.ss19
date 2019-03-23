202c3f0b5f84284838438a6cc666e2776e39450f
Philipp Gewinner <philipp.gewinner@alysis.at>
2019-03-23 11:53:29 +0100

    - TS 19 deleted unnecessary imports in tests

--- src/test/java/at/ac/tuwien/sepm/assignment/individual/e1207708/service/CalcServiceTest.java
+++ src/test/java/at/ac/tuwien/sepm/assignment/individual/e1207708/service/CalcServiceTest.java
@@ -3,7 +3,6 @@ package at.ac.tuwien.sepm.assignment.individual.e1207708.service;
 import at.ac.tuwien.sepm.assignment.individual.e1207708.persistence.exceptions.PersistenceException;
 import at.ac.tuwien.sepm.assignment.individual.e1207708.persistence.util.DBConnectionManager;
 import at.ac.tuwien.sepm.assignment.individual.e1207708.service.exceptions.BadRequestException;
-import at.ac.tuwien.sepm.assignment.individual.e1207708.service.exceptions.ServiceException;
 import at.ac.tuwien.sepm.assignment.individual.e1207708.service.impl.CalcService;
 import org.junit.After;
 import org.junit.Test;
